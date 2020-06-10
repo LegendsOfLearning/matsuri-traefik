6c42326d007693d7acb687596e5a3300b6e37da5
Ho-Sheng Hsiao <hosh@legendsoflearning.com>
2020-06-10 01:20:26 -0700

    [cmd] added apply, create, delete, diff, recreate

--- lib/matsuri/traefik/cmd.rb
+++ lib/matsuri/traefik/cmd.rb
@@ -6,6 +6,15 @@ require 'matsuri'
 module Matsuri
   module Traefik
     module Cmd
+      module Apply
+        extend ActiveSupport::Concern
+
+        included do
+          desc 'traefik/ingress_route NAME', 'appy an ingress_route'
+          apply_cmd_for 'traefik_ingress_route'
+          map "traefik/ingress-route" => :traefik_ingress_route
+        end
+      end
 
       module Create
         extend ActiveSupport::Concern
@@ -14,7 +23,6 @@ module Matsuri
           desc 'traefik/ingress_route NAME', 'create an ingress_route'
           create_cmd_for 'traefik_ingress_route'
           map "traefik/ingress-route" => :traefik_ingress_route
-          # map sts: :stateful_set
         end
       end
 
@@ -27,11 +35,46 @@ module Matsuri
           map "traefik/ingress-route" => :traefik_ingress_route
         end
       end
+
+      module Delete
+        extend ActiveSupport::Concern
+
+        included do
+          desc 'traefik/ingress_route NAME', 'delete an ingress_route'
+          delete_cmd_for 'traefik_ingress_route'
+          map "traefik/ingress-route" => :traefik_ingress_route
+        end
+      end
+
+      module Diff
+        extend ActiveSupport::Concern
+
+        included do
+          desc 'traefik/ingress_route NAME', 'diff an ingress_route'
+          diff_cmd_for 'traefik_ingress_route'
+          map "traefik/ingress-route" => :traefik_ingress_route
+        end
+      end
+
+      module Recreate
+        extend ActiveSupport::Concern
+
+        included do
+          desc 'traefik/ingress_route NAME', 'recreate an ingress_route'
+          recreate_cmd_for 'traefik_ingress_route'
+          map "traefik/ingress-route" => :traefik_ingress_route
+        end
+      end
     end
   end
 end
 
+Matsuri::Cmds::Apply.send(:include, Matsuri::Traefik::Cmd::Apply)
+Matsuri::Cmds::Create.send(:include, Matsuri::Traefik::Cmd::Create)
+Matsuri::Cmds::Delete.send(:include, Matsuri::Traefik::Cmd::Delete)
+Matsuri::Cmds::Recreate.send(:include, Matsuri::Traefik::Cmd::Recreate)
 Matsuri::Cmds::Show.send(:include, Matsuri::Traefik::Cmd::Show)
+Matsuri::Cmds::Diff.send(:include, Matsuri::Traefik::Cmd::Diff)
 
 
 
